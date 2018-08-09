343255b75f940b1cd2f6bb18279678da84c0f5e0
Aaron King <lbdrking2002@yahoo.co.uk>
2018-08-09 15:20:13 +0100

    added to content and resized image

--- src/Content.js
+++ src/Content.js
@@ -9,9 +9,9 @@ class Content extends Component {
      <h3>Who am I?</h3>
      <p>My name is Aaron King and I am a Front-End Software Engineer,I am based in the South East of London.</p>
      <h3>Motivation</h3>
-     <p>I love to code, I want to be able to put ideas into practice</p>
+     <p>I love to code, I want to be able to put ideas into practice, I also wanted to work with a team where we are making a real difference to the public</p>
      <h3>Interests and Hobbies</h3>
-     <p>I love Snooker and motorbikes, I have just chaged focus towards Harley Davidson</p>
+     <p>I love to learn new languages, I am very interested in blockchain. I love Snooker and motorbikes, I have just chaged focus towards Harley Davidson</p>
     </div>
     );
   }
