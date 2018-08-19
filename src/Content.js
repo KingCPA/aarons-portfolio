a676ab0433da443f08f84e62a0ec140fe55e04af
Aaron King <lbdrking2002@yahoo.co.uk>
2018-08-19 19:37:39 +0100

    updated content page

--- src/Content.js
+++ src/Content.js
@@ -11,7 +11,7 @@ class Content extends Component {
      <h3>Motivation</h3>
      <p>I love to code, I want to be able to put ideas into practice, I also wanted to work with a team where we are making a real difference to the public</p>
      <h3>Interests and Hobbies</h3>
-     <p>I love to learn new languages, I am very interested in blockchain. I love Snooker and motorbikes, I have just chaged focus towards Harley Davidson</p>
+     <p>I love to learn new languages, I am very interested in blockchain. I love Snooker and motorbikes, while I have always loved superbikes I have now chaged focus towards Harley Davidson</p>
     </div>
     );
   }
